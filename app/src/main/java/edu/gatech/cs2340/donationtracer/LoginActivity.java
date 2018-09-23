044a00b3472e2e8c674b3a9a2d68084b271032e5
Alexa Flesch <alexaflesch@gmail.com>
2018-09-23 14:52:01 -0400

    added login screen

--- app/src/main/java/edu/gatech/cs2340/donationtracer/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracer/LoginActivity.java
@@ -1,4 +1,13 @@
 package edu.gatech.cs2340.donationtracer;
 
-public class LoginActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
