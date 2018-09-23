d524ad51383ec542d73af564c0b92afaf36713b7
Eva Huang <ehuang42@gatech.edu>
2018-09-23 15:35:49 -0400

    login edit text

--- app/src/main/java/edu/gatech/cs2340/donationtracer/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracer/LoginActivity.java
@@ -1,7 +1,11 @@
 package edu.gatech.cs2340.donationtracer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText userName = (EditText) findViewById(R.id.username_input);
+        EditText password = (EditText) findViewById(R.id.password_input);
+
+        String user = userName.toString().trim();
+        String pass = password.toString().trim();
+
     }
 }
--- app/src/main/java/edu/gatech/cs2340/donationtracer/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracer/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package edu.gatech.cs2340.donationtracer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        Button loginBtn = (Button) findViewById(R.id.button_login);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // write your button click code here
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+
     }
 }
