c80e0d7ea7817b7dc9d380a0808e77893abc86e4
Helena Makendengue <HelenaMakens1013@gmail.com>
2018-09-23 15:34:30 -0400

    added functionality to welcome screen login button

--- app/src/main/java/edu/gatech/cs2340/donationtracer/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracer/WelcomeActivity.java
@@ -2,6 +2,10 @@ package edu.gatech.cs2340.donationtracer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button btn = (Button)findViewById(R.id.button_login);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
