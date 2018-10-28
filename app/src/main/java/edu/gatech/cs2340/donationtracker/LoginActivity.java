d4dae30af5c44a9f296b707ef39fddae58010e2a
Eva Huang <ehuang42@gatech.edu>
2018-10-28 13:22:05 -0400

    added authentication

--- app/src/main/java/edu/gatech/cs2340/donationtracker/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/LoginActivity.java
@@ -1,12 +1,21 @@
 package edu.gatech.cs2340.donationtracker;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,7 +25,25 @@ public class LoginActivity extends AppCompatActivity {
     private Button cancel;
 
     public static String username;
+    private FirebaseAuth mAuth;
 
+    private void startAuth(String username, String pass) {
+        mAuth.signInWithEmailAndPassword(username, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        }
+                });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +53,9 @@ public class LoginActivity extends AppCompatActivity {
         usernameInput = (EditText) findViewById(R.id.username_input);
         passwordInput = (EditText) findViewById(R.id.password_input);
         cancel = (Button) findViewById(R.id.button_cancel);
+        mAuth = FirebaseAuth.getInstance();
+
+
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,12 +64,7 @@ public class LoginActivity extends AppCompatActivity {
                 String userName = usernameInput.getText().toString().trim();
                 String password = passwordInput.getText().toString().trim();
 
-                if (RegistrationActivity.userDatabase.containsKey(userName)
-                        && RegistrationActivity.userDatabase.get(userName).getPassword().equals(password)) {
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                } else {
-                    Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
-                }
+                startAuth(userName, password);
             }
         });
 
