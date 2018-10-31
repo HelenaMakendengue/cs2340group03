c1e729673056f29ab5a2c0ce824ddf39c1afbd4f
67sivad <mdwilliams67@gmail.com>
2018-10-31 13:51:04 -0400

    Added lcoation spinner, but it is not filled with locations yet

--- app/src/main/java/edu/gatech/cs2340/donationtracker/SearchActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/SearchActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -19,6 +20,7 @@ public class SearchActivity extends AppCompatActivity {
 
     private ArrayList<Item> searched;
     private ArrayList<Item> master;
+    private ArrayList<String> locationNames;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private ItemRecyclerAdapter adapter;
@@ -32,18 +34,28 @@ public class SearchActivity extends AppCompatActivity {
         EditText searchBar = findViewById(R.id.search_bar);
         Button searchButton = findViewById(R.id.search_button);
         Spinner categories = findViewById(R.id.category_search);
+        Spinner locations = findViewById(R.id.location_spinner);
         RadioButton textOption = findViewById(R.id.text_option);
         RadioButton spinnerOption = findViewById(R.id.spinner_option);
+        CheckBox locationActive = findViewById(R.id.location_check);
+
         searched = new ArrayList<>();
         master = new ArrayList<>();
+        locationNames = new ArrayList<>();
 
         master.add(new Item(null, null, "shoe", "big shoe", 65.0, Category.Clothing));
 
 
-        //set up spinner
+        //set up spinners
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, Category.values());
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categories.setAdapter(spinnerAdapter);
+
+        ArrayAdapter<String> spinnerAdapter2 = new ArrayAdapter(this,android.R.layout.simple_spinner_item, locationNames);
+        spinnerAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        locations.setAdapter(spinnerAdapter2);
+
+
 
         //set up recycler view
         recyclerView = findViewById(R.id.recyclerView);
@@ -57,22 +69,40 @@ public class SearchActivity extends AppCompatActivity {
             public void onClick(View v) {
                 searched.clear();
 
-                if (textOption.isChecked()) {
-                    String term = searchBar.getText().toString().toLowerCase();
+                if (locationActive.isChecked()) {
+                    if (textOption.isChecked()) {
+                        String term = searchBar.getText().toString().toLowerCase();
 
-                    for (Item item: master) {
-                        if (item.getShortDesc().toLowerCase().contains(term)) {
-                            searched.add(item);
+                        for (Item item : master) {
+                            if (item.getShortDesc().toLowerCase().contains(term) && item.getLocation().getName().equals(locations.getSelectedItem())) {
+                                searched.add(item);
+                            }
+                        }
+                    } else if (spinnerOption.isChecked()) {
+                        for (Item item : master) {
+                            if (item.getCategory().equals(categories.getSelectedItem()) && item.getLocation().getName().equals(locations.getSelectedItem())) {
+                                searched.add(item);
+                            }
                         }
                     }
-                } else if (spinnerOption.isChecked()) {
-                    for (Item item: master) {
-                        if (item.getCategory().equals(categories.getSelectedItem())) {
-                            searched.add(item);
+                } else {
+                    if (textOption.isChecked()) {
+                        String term = searchBar.getText().toString().toLowerCase();
+
+                        for (Item item : master) {
+                            if (item.getShortDesc().toLowerCase().contains(term)) {
+                                searched.add(item);
+                            }
+                        }
+                    } else if (spinnerOption.isChecked()) {
+                        for (Item item : master) {
+                            if (item.getCategory().equals(categories.getSelectedItem())) {
+                                searched.add(item);
+                            }
                         }
                     }
+
                 }
-
 
                 if (searched.size() == 0) {
                     Toast.makeText(getApplicationContext(), "No Items Matched", Toast.LENGTH_LONG).show();
