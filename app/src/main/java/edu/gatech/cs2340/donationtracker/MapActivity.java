61d78c9a11480580cc605e6581bbac42a2e55ae8
Helena Makendengue <HelenaMakens1013@gmail.com>
2018-11-01 11:13:00 -0400

    working on location persistence

--- app/src/main/java/edu/gatech/cs2340/donationtracker/MapActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/MapActivity.java
@@ -18,6 +18,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -38,9 +39,17 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Example: Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+//        for (location : locationArray) {
+//
+//        }
+
+
+
     }
 }
