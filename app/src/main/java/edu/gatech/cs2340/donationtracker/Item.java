e6e8f666d1ec5b98db557ab07106387cf453b857
Helena Makendengue <HelenaMakens1013@gmail.com>
2018-11-06 12:20:23 -0500

    added javadocs to item class

--- app/src/main/java/edu/gatech/cs2340/donationtracker/Item.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/Item.java
@@ -1,6 +1,5 @@
 package edu.gatech.cs2340.donationtracker;
 
-//import java.sql.Timestamp;
 import java.util.ArrayList;
 
 public class Item {
@@ -14,9 +13,6 @@ public class Item {
     private double dollarValue;
     private Category category;
 
-    // --> Comments (optional)
-    // --> Picture (optional)
-
     public Item (String timestamp, Location location, String shortDesc, String fullDesc, Double dollarValue, Category category) {
         this.timestamp = timestamp;
         this.location = location;
@@ -28,36 +24,134 @@ public class Item {
 
     public Item() {}
 
+    /**
+     * This method gets and returns the timestamp for
+     * the item's creation.
+     *
+     * @return the timestamp for the item's creation.
+     */
     public String getTimestamp() {
         return timestamp;
     }
-    public void setTimestamp(String timestamp) { this.timestamp = timestamp; }
 
+    /**
+     * This method sets the timestamp for the item's
+     * creation.
+     *
+     * @param timestamp a time in which the item was created
+     */
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    /**
+     * The method gets and returns the location
+     * for this item.
+     *
+     * @return the location at which the item is available
+     */
     public Location getLocation() {
         return location;
     }
+
+    /**
+     * This method sets the location for the item.
+     *
+     * @param location a location at which the item
+     *                 is available
+     */
     public void setLocation(Location location) { this.location = location; }
 
+    /**
+     * This method gets and returns the short
+     * description about the item.
+     *
+     * @return the shortDesc about the item
+     */
     public String getShortDesc() {
         return shortDesc;
     }
+
+    /**
+     * This method sets the short description about an item
+     *
+     * @param shortDesc a short description about an item
+     */
     public void setShortDesc(String shortDesc) { this.shortDesc = shortDesc; }
 
-    public String getFullDesc() { return fullDesc; }
-    public void setFullDesc(String fullDesc) { this.fullDesc = fullDesc; }
+    /**
+     * This method gets and returns a full description
+     * about the item.
+     *
+     * @return the fullDesc about the item
+     */
+    public String getFullDesc() {
+        return fullDesc;
+    }
 
+    /**
+     * This method sets the full description about
+     * an item.
+     *
+     * @param fullDesc a full description about an item
+     */
+    public void setFullDesc(String fullDesc) {
+        this.fullDesc = fullDesc;
+    }
+
+    /**
+     * This method gets and returns the dollar
+     * value of the item.
+     *
+     * @return dollarValue the dollar value of the item
+     */
     public Double getDollarValue() {
         return dollarValue;
     }
-    public void setDollarValue(double dollarValue) { this.dollarValue = dollarValue; }
 
-    public Category getCategory() { return category; }
-    public void setCategory(Category category) { this.category = category; }
+    /**
+     * This method returns sets the dollar value of the item
+     *
+     * @param dollarValue a dollar value for the item
+     */
+    public void setDollarValue(double dollarValue) {
+        this.dollarValue = dollarValue;
+    }
 
+    /**
+     * This method gets and returns the category the item
+     * falls under
+     *
+     * @return category the category that an item falls under
+     */
+    public Category getCategory() {
+        return category;
+    }
+
+    /**
+     * This method sets the category that the item falls under
+     *
+     * @param category a category that the item falls under
+     */
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    /**
+     * This method returns a list of store items
+     *
+     * @return itemList
+     */
     public static ArrayList<Item> getItemList() {
         return itemList;
     }
 
+    /**
+     * This method represent the item in the form
+     * of a String using the short description
+     *
+     * @return shortDesc the short description of an item
+     */
     @Override
     public String toString() {
         return shortDesc;
