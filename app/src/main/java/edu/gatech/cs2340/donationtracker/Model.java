400d971c89036dd96ad5b44f06dfd403aa0c9cc1
Helena Makendengue <HelenaMakens1013@gmail.com>
2018-11-14 10:22:18 -0500

    fixed more than 80 characters long error in my JUnit

--- app/src/main/java/edu/gatech/cs2340/donationtracker/Model.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/Model.java
@@ -106,30 +106,35 @@ public class Model {
     }
 
     public static void LocationReaderModel() {
-        modelDB.add(new Location("1", "AFD Station", "4,33.75416",
-                "-84.37742", "309 EDGEWOOD AVE SE, Atlanta, GA 30332",
-                LocationType.DROP_OFF_ONLY, "(404) 555 - 3456",
-                "www.afd04.atl.ga"));
-        modelDB.add(new Location("2", "BOYS & GILRS CLUB W.W. WOOLFOLK", "33.73182",
-                "-84.43971", "1642 RICHLAND RD, Atlanta, GA 30332",
-                LocationType.STORE, "(404) 555 - 1234",
-                "www.bgc.wool.ga"));
-        modelDB.add(new Location("3", "PATHWAY UPPER ROOM CHRISTIAN MINISTRIES", "33.70866",
-                "-84.41853", "683 SYLVAN RD, Atlanta, GA 30332",
-                LocationType.WAREHOUSE, "(404) 555 - 5432",
-                "www.pathways.org"));
-        modelDB.add(new Location("4", "PAVILION OF HOPE INC", "33.80129",
-                "-84.25537", "3558 EAST PONCE DE LEON AVE, SCOTTDALE, GA 30079",
-                LocationType.WAREHOUSE, "(404) 555 - 8765",
-                "www.pathways.org"));
-        modelDB.add(new Location("5", "D&D CONVENIENCE STORE", "33.71747",
-                "-84.2521", "2426 COLUMBIA DRIVE, DECATUR, GA 30034",
-                LocationType.DROP_OFF_ONLY, "(404) 555 - 9876",
-                "www.ddconv.com"));
-        modelDB.add(new Location("6", "KEEP NORTH FULTON BEAUTIFUL", "33.96921",
-                "-84.3688", "470 MORGAN FALLS RD, Sandy Springs, GA 30302",
-                LocationType.STORE, "(770) 555 - 7321",
-                "www.knfb.org"));
+
+        if (modelDB.isEmpty()) {
+            modelDB.add(new Location("1", "AFD Station", "4,33.75416",
+                    "-84.37742", "309 EDGEWOOD AVE SE, Atlanta, GA 30332",
+                    LocationType.DROP_OFF_ONLY, "(404) 555 - 3456",
+                    "www.afd04.atl.ga"));
+            modelDB.add(new Location("2", "BOYS & GILRS CLUB W.W. WOOLFOLK",
+                    "33.73182",
+                    "-84.43971", "1642 RICHLAND RD, Atlanta, GA 30332",
+                    LocationType.STORE, "(404) 555 - 1234",
+                    "www.bgc.wool.ga"));
+            modelDB.add(new Location("3", "PATHWAY UPPER ROOM CHRISTIAN MINISTRIES",
+                    "33.70866",
+                    "-84.41853", "683 SYLVAN RD, Atlanta, GA 30332",
+                    LocationType.WAREHOUSE, "(404) 555 - 5432",
+                    "www.pathways.org"));
+            modelDB.add(new Location("4", "PAVILION OF HOPE INC", "33.80129",
+                    "-84.25537", "3558 EAST PONCE DE LEON AVE, SCOTTDALE, GA 30079",
+                    LocationType.WAREHOUSE, "(404) 555 - 8765",
+                    "www.pathways.org"));
+            modelDB.add(new Location("5", "D&D CONVENIENCE STORE", "33.71747",
+                    "-84.2521", "2426 COLUMBIA DRIVE, DECATUR, GA 30034",
+                    LocationType.DROP_OFF_ONLY, "(404) 555 - 9876",
+                    "www.ddconv.com"));
+            modelDB.add(new Location("6", "KEEP NORTH FULTON BEAUTIFUL", "33.96921",
+                    "-84.3688", "470 MORGAN FALLS RD, Sandy Springs, GA 30302",
+                    LocationType.STORE, "(770) 555 - 7321",
+                    "www.knfb.org"));
+        }
     }
 
 
