79649484e32c8f3d5ee2d6a5e1d583cfefdf26b0
mahimachander <mchander6@gatech.edu>
2018-11-29 17:28:24 -0500

    Added guest user
    Added buttons for guest user and google log in

--- app/src/main/java/edu/gatech/cs2340/donationtracker/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/WelcomeActivity.java
@@ -5,11 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 /**
  * The welcome activity initiates the welcome screen.
  * Links to login or registration activity.
  */
 public class WelcomeActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,11 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         Button loginBtn = findViewById(R.id.button_login);
         Button registerBtn = findViewById(R.id.button_register);
+        Button guestBtn = findViewById(R.id.button_guest);
+        Button googleBtn = findViewById(R.id.button_google);
+
+        mAuth = FirebaseAuth.getInstance();
+
 
         loginBtn.setOnClickListener(view -> {
             // write your button click code here
@@ -28,5 +38,15 @@ public class WelcomeActivity extends AppCompatActivity {
             startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
         });
 
+        guestBtn.setOnClickListener(view -> {
+            // write your button click code here
+            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+        });
+
+        //googleBtn.setOnClickListener(view -> {
+        //    startActivity(new Intent(WelcomeActivity.this, ??????));
+        //        });
     }
+
+
 }
